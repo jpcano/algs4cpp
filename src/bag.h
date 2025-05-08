65900bed7affb846cd694af5d2a6c7ab561bb0a5
Jesus Cano <jcanovel@gmail.com>
2025-05-08 01:06:48 +0100

    Add iterator to bag

--- src/bag.h
+++ src/bag.h
@@ -2,6 +2,48 @@
 
 template <typename T>
 class Bag {
+ private:
+  class Node {
+   public:
+    T item;
+    Node* next;
+    Node(T item) : item(item), next(nullptr) {}
+  };
+
+  Node* first;
+
+  class IteratorBag {
+   public:
+    using iterator_category = std::forward_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = Node;
+    using pointer = Node*;
+    using reference = Node&;
+
+    IteratorBag(Node* ptr) : m_ptr(ptr) {}
+
+    T& operator*() const { return m_ptr->item; }
+    pointer operator->() { return m_ptr; }
+    IteratorBag& operator++() {
+      m_ptr = m_ptr->next;
+      return *this;
+    }
+    IteratorBag operator++(int) {
+      IteratorBag tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+    friend bool operator==(const IteratorBag& a, const IteratorBag& b) {
+      return a.m_ptr == b.m_ptr;
+    };
+    friend bool operator!=(const IteratorBag& a, const IteratorBag& b) {
+      return a.m_ptr != b.m_ptr;
+    };
+
+   private:
+    Node* m_ptr;
+  };
+
  public:
   Bag() : first(nullptr) {}
   ~Bag() {
@@ -18,12 +60,6 @@ class Bag {
     first->next = oldfirst;
   }
 
- private:
-  class Node {
-   public:
-    T item;
-    Node* next;
-    Node(T item) : item(item), next(nullptr) {}
-  };
-  Node* first;
+  IteratorBag begin() { return IteratorBag(first); }
+  IteratorBag end() { return IteratorBag(nullptr); }
 };
--- src/bag.test.cpp
+++ src/bag.test.cpp
@@ -3,8 +3,16 @@
 #include <catch2/catch_test_macros.hpp>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 TEST_CASE("bag.cpp test", "[bag.cpp]") {
   Bag<std::string> bag;
+  std::vector<std::string> bag_content;
+
   bag.add("Hello");
+  bag.add("World");
+  for (auto item : bag) bag_content.push_back(item);
+
+  std::vector<std::string> expected = {"World", "Hello"};
+  REQUIRE(bag_content == expected);
 }
